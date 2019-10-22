64a09d57c433acb3897f018a86a98ff2ab5ad689
Deon Choi <deon.choi@gmail.com>
2019-10-21 22:33:29 -0700

    Updates

---
+++ precache-manifest.19163ce2a441d5166ad899e1a2fc25ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e851bb0cefca891a827c4a9cb82927c5",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "7303669c0eecc66bad97",
+    "url": "/tic-tac-toe/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ab521776845c7e94abfe",
+    "url": "/tic-tac-toe/static/css/main.fe9e7212.chunk.css"
+  },
+  {
+    "revision": "7303669c0eecc66bad97",
+    "url": "/tic-tac-toe/static/js/2.42a932f7.chunk.js"
+  },
+  {
+    "revision": "ab521776845c7e94abfe",
+    "url": "/tic-tac-toe/static/js/main.02e58269.chunk.js"
+  },
+  {
+    "revision": "385629666264212be688",
+    "url": "/tic-tac-toe/static/js/runtime-main.9b00db00.js"
+  },
+  {
+    "revision": "027b1ef283de0ae7819c30a7a8ad3b9a",
+    "url": "/tic-tac-toe/static/media/background.027b1ef2.jpg"
+  }
+]);
